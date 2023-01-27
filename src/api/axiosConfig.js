0ea4bf8fab3eb5c6bc56393ab6f2ff3cbad6b544
s_timokhin <s_timokhin@wargaming.net>
2023-01-27 16:43:54 +0200

    change host to appache

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+let apache2_host = process.env.REACT_APP_APACHE2_HOST || "http://localhost";
+
 export default axios.create({
-  baseURL: "https://c5a8-62-228-157-150.eu.ngrok.io",
+  // baseURL: "https://c5a8-62-228-157-150.eu.ngrok.io",
+  baseURL: apache2_host,
   headers: { "ngrok-skip-browser-warning": "true" },
 });
