037881b3afc26181782c90f675aef8d562b572a7
Sergei Timokhin <91485516+b1gslick@users.noreply.github.com>
2023-02-03 21:22:04 +0200

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -4,5 +4,5 @@ let apache2_host = process.env.REACT_APP_APACHE2_HOST || "http://localhost:8081"
 
 export default axios.create({
   baseURL: "https://bd6e-62-228-157-150.ngrok.io",
-  headers: { "ngrok-skip-browser-warning": "true" },
+  headers: { "ngrok-skip-browser-warning": "true", "Access-Control-Allow-Origin": "*" },
 });
