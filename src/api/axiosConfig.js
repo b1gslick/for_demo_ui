d264b71d9cdffd86ce4096cc8368079eac82e604
Sergei Timokhin <91485516+b1gslick@users.noreply.github.com>
2023-02-07 19:52:32 +0200

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,6 +3,6 @@ import axios from "axios";
 let apache2_host = process.env.REACT_APP_APACHE2_HOST || "http://localhost:8081";
 
 export default axios.create({
-  baseURL: "https://bd6e-62-228-157-150.ngrok.io",
+  baseURL: "http://0f2f-62-228-157-150.ngrok.io",
   headers: { "ngrok-skip-browser-warning": "true", "Access-Control-Allow-Origin": "*" },
 });
